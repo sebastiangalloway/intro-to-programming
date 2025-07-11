51ccd0b872d040c7ad355916c32e0db201569f57
sebastiangalloway <sgalloway111@gmail.com>
2025-07-11 17:20:44 -0400

    week 1 step 10 calc

--- week-1/StringCalculatorKata-main/StringCalculatorKata-main/StringCalculator/Calculator.cs
+++ week-1/StringCalculatorKata-main/StringCalculatorKata-main/StringCalculator/Calculator.cs
@@ -1,22 +1,43 @@
 ﻿
+using System.Text;
+
 public class Calculator
 {
     public int Add(string numbers)
     {
         int output = 0;
-        List<char> delimiters = [ ',', '\n' ];
+        bool isNegative = false;
+        List<int> negatives = new List<int>();
+
+        List<string> delimiters = new List<string>() { ",", "\n" };
 
         if (numbers.Length >= 3 && numbers.Substring(0, 2).Equals("//"))
         {
-            delimiters.Add(numbers[2]);
+            delimiters.Add(numbers[2].ToString());
             numbers = numbers[3..];
         }
+        else if(numbers.StartsWith("//[") && numbers.LastIndexOf(']') > 3)
+        {
+            delimiters.Add(numbers.Substring(3, numbers.LastIndexOf(']') - 1));
+            numbers = numbers.Substring(numbers.LastIndexOf(']') + 1);
+        }
 
-        string[] splitNumbers = numbers.Split(delimiters.ToArray());
+        string[] splitNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.None);
         foreach (string number in splitNumbers)
         {
-            output += number == "" ? 0 : int.Parse(number);
+            int temp = number == "" ? 0 : int.Parse(number);
+            if (temp < 0)
+            {
+                isNegative = true;
+                negatives.Add(temp);
+                continue;
+            }
+            if (temp <= 1000)
+            {
+                output += temp;
+            }
         }
-        return output;
+        if (isNegative) throw new ArgumentException(String.Join(", ", negatives.ToArray()));
+        else return output;
     }
 }
--- week-1/StringCalculatorKata-main/StringCalculatorKata-main/StringCalculator/CalculatorTests.cs
+++ week-1/StringCalculatorKata-main/StringCalculatorKata-main/StringCalculator/CalculatorTests.cs
@@ -18,7 +18,6 @@ public class CalculatorTests
     [Theory]
     [InlineData("3", 3)]
     [InlineData("4", 4)]
-    [InlineData("-100000", -100000)]
     public void CanAddSingleNumber(string value, int expected)
     {
         var calculator = new Calculator();
@@ -30,7 +29,6 @@ public class CalculatorTests
     [InlineData("1,2,3", 6)]
     [InlineData("1,", 1)]
     [InlineData("10,20,30", 60)]
-    [InlineData("-10,20,30", 40)]
     [InlineData("1,2", 3)]
     [InlineData("1,2,3,4,5,6,7,8,9", 45)]
     public void CanAddMultipleNumbers(string values, int expected)
@@ -45,6 +43,55 @@ public class CalculatorTests
     [InlineData("//#\n1#2,3\n1", 7)]
     [InlineData("//#\n1#2#3", 6)]
     public void CanUseMultipleDelimiters(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,-2,3")]
+    [InlineData("-1,2,-3")]
+
+    public void ThrowsWhenNegative(string values)
+    {
+        var calculator = new Calculator();
+        Assert.Throws<ArgumentException>(() => calculator.Add(values));
+    }
+
+
+    [Theory]
+    [InlineData("1,-2,3", "-2")]
+    [InlineData("-1,2,-3", "-1, -3")]
+    [InlineData("//;\n1;-2", "-2")]
+    public void ThrowsMessageWhenNegative(string values, string expectedMessage)
+    {
+        var calculator = new Calculator();
+        var exception = Assert.Throws<ArgumentException>(() => calculator.Add(values));
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+
+
+    [Theory]
+    [InlineData("1,2,3", 6)]
+    [InlineData("1,", 1)]
+    [InlineData("10,2000,30", 40)]
+    [InlineData("1000,2", 1002)]
+    [InlineData("1,2,3,4,5,6,7,8,9000", 36)]
+    public void IgnoreOver1000(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+
+
+
+    [Theory]
+    [InlineData("\"//[***]", 3)]
+    [InlineData("//#\n1#2,3\n1", 7)]
+    [InlineData("//#\n1#2#3", 6)]
+    public void CanUseStringDelimiters(string values, int expected)
     {
         var calculator = new Calculator();
         var result = calculator.Add(values);
